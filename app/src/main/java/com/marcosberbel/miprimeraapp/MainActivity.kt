80802f5399126fb3a614d82d692b1c7aa0e664e2
CosMark1 <marber24@alu.centroafuera.es>
2021-09-14 11:15:03 +0200

    Nuevos archivos

--- app/src/main/java/com/marcosberbel/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/marcosberbel/miprimeraapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.marcosberbel.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("MainActivity","Se ha creado correctamente")
 
     }
 }
